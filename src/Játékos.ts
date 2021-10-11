5bc3de39ca97a4cef428f9755d91fce50d3943e5
nitslaszlo <nitslaszlo@gmail.com>
2021-10-11 15:41:58 +0200

    Játékos osztály start

---
+++ src/Játékos.ts
@@ -0,0 +1,14 @@
+export default class Játékos {
+    // Privát adattagok:
+    #név: string; // privát mező TS 3.8.0-ás verziótól használható
+    #tippek: number[] = [];
+
+    // Kódtagok:
+    constructor(sor: string) {
+        const m: string[] = sor.split(" ");
+        this.#név = m[0];
+        for (let i = 1; i < m.length; i++) {
+            this.#tippek.push(parseInt(m[i]));
+        }
+    }
+}
